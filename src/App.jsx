14eff3c171775def0bde78c0086d6e28f453d816
zk <596718356@qq.com>
2020-12-31 23:45:11 +0800

    useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,31 @@
-import { useEffect, useState } from 'react'
-import React, { Component } from 'react'
+import { useState, createContext, useContext } from 'react'
 
-class App extends Component {
-  state = {
-    count: 0,
-    size: {
-      width: document.documentElement.clientWidth,
-      height: document.documentElement.clientHeight
-    }
-  }
-  onResize = () => {
-    this.setState({
-      size: {
-        width: document.documentElement.clientWidth,
-        height: document.documentElement.clientHeight
-      }
-    })
-  }
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.onResize, false)
-  }
-  componentDidMount() {
-    window.addEventListener('resize', this.onResize, false)
-    document.title = this.state.count
-  }
-  render() {
-    const { count, size } = this.state
-    return (
-      <div>
-        <button
-          onClick={() => {
-            this.setState({
-              count: count + 1
-            })
-          }}
-        >
-          按钮
-        </button>
-        <div>count： {count}</div>
-        <div>
-          width: {size.width} height:{size.height}
-        </div>
-      </div>
-    )
-  }
+const CounterContext = createContext()
+
+function Child() {
+  console.log(useContext(CounterContext))
+  const counter = useContext(CounterContext)
+  return <h2>{counter}</h2>
 }
-
-function App2() {
-  // const [count, setCount] = useState(0)
-  // 延迟初始化，只执行一次
+function App() {
   const [count, setCount] = useState(0)
-  const [size, setSize] = useState({
-    width: document.documentElement.clientWidth,
-    height: document.documentElement.clientHeight
-  })
-
-  const onResize = () => {
-    setSize({
-      width: document.documentElement.clientWidth,
-      height: document.documentElement.clientHeight
-    })
-  }
-  useEffect(() => {
-    console.log('count', count)
-  }, [count])
-  useEffect(() => {
-    document.title = count
-  })
-  // componentDidUpdate, componentDidMount
-  useEffect(() => {
-    window.addEventListener('resize', onResize, false)
-    // return 在组件卸载或组件重新渲染时执行
-    return () => {
-      window.removeEventListener('resize', onResize, false)
-    }
-  }, [])
-
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>按钮</button>
-      <div>count： {count}</div>
+      <div>context</div>
+      <button
+        onClick={() => {
+          setCount(count + 1)
+        }}
+      >
+        button
+      </button>
+      <h1>{count}</h1>
       <div>
-        width: {size.width} height:{size.height}
+        <CounterContext.Provider value={count}>
+          <Child />
+        </CounterContext.Provider>
       </div>
     </div>
   )
 }
-export default App2
+export default App
