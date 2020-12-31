ce768a6382c8fbc9bf8ca780a57c81de3fdff75c
zk <596718356@qq.com>
2020-12-31 14:47:45 +0800

    contextType

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
-import { useState, createContext } from 'react'
+import { Component, useState, createContext } from 'react'
 const BetteryContext = createContext() // 括号中为默认值
-const OnlineContext = createContext()
 
-function Leaf() {
-  return (
-    <BetteryContext.Consumer>
-      {(count) => (
-        <OnlineContext.Consumer>
-          {(online) => (
-            <h1>
-              count:{count}
-              online: {String(online)}
-            </h1>
-          )}
-        </OnlineContext.Consumer>
-      )}
-    </BetteryContext.Consumer>
-  )
+class Leaf extends Component {
+  static contextType = BetteryContext
+  render() {
+    const count = this.context
+    return <h1>count:{count}</h1>
+  }
 }
 
 function Middle() {
@@ -25,16 +15,12 @@ function Middle() {
 function App() {
   // 声明一个叫 "count" 的 state 变量
   const [count, setCount] = useState(60)
-  const [online, setOnline] = useState(false)
   return (
     <BetteryContext.Provider value={count}>
-      <OnlineContext.Provider value={online}>
-        <div className="App">
-          <button onClick={() => setCount(count + 1)}>Click count</button>
-          <button onClick={() => setOnline(!online)}>Click online</button>
-          <Middle />
-        </div>
-      </OnlineContext.Provider>
+      <div className="App">
+        <button onClick={() => setCount(count + 1)}>Click count</button>
+        <Middle />
+      </div>
     </BetteryContext.Provider>
   )
 }
